14084b7f680c35685cd34bad5ebfb73dd705ac66
Tamir <shklazt@gmail.com>
2018-07-02 22:48:30 +0200

    First Commit

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import {model, Entity, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  email: string;
+}
